7b7e1eec816901dfaa37558d3cc92959018a2d7b
Intakhab2004 <ikbalam4648@gmail.com>
2025-08-17 19:35:33 +0530

    Resolved theme hydration problem

--- src/components/common/ThemeToggle.tsx
+++ src/components/common/ThemeToggle.tsx
@@ -3,10 +3,15 @@
 import { Moon } from "lucide-react"
 import { Sun } from "lucide-react"
 import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 
 
 export const ThemeToggle = () => {
     const {theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => setMounted(true), []);
+    if(!mounted) return null;
 
     return(
         <div
