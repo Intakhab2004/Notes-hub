7bc9bfc8a9754d3c02c0053f8f808523bfa44cbd
Intakhab2004 <ikbalam4648@gmail.com>
2025-09-01 17:38:51 +0530

    Resolved file handling issues

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	api: {
+		bodyParser: {
+			sizeLimit: "30mb"
+		}
+	},
+	
 	webpack: (config, { isServer }) => {
 		if (isServer) {
 			config.resolve.alias.canvas = false; // prevent canvas from being required server-side
